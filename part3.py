7e3938af9c0ab6f3e78591709926dd84d765af8a
Boaz Shvartzman <boaz@slatescience.com>
2018-06-16 12:31:21 +0300

    Some fixes

--- part3.py
+++ part3.py
@@ -4,9 +4,10 @@ from part2 import RegressionTreeEnsemble, RegressionTreeNode, RegressionTree
 
 class CART(object):
 
-    def __init__(self, max_depth, min_node_size):
+    def __init__(self, max_depth, min_node_size, num_thresholds):
         self._max_depth = max_depth
         self._min_node_size = min_node_size
+        self._num_thresholds = num_thresholds
 
     def fit(self, train_set):
 
@@ -62,11 +63,11 @@ class CART(object):
 
 class GBRT(object):
 
-    def __init__(self, num_of_basis_functions, num_of_leaves, min_node_size, shrinkage, subsampling):
+    def __init__(self, num_of_basis_functions, num_of_leaves, min_node_size, shrinkage, subsampling, num_thresholds):
         self._num_of_basis_functions = num_of_basis_functions
         self._shrinkage = shrinkage
         self._subsampling = subsampling
-        self._cart = CART(np.log2(num_of_leaves) + 1, min_node_size)
+        self._cart = CART(np.log2(num_of_leaves) + 1, min_node_size, num_thresholds)
         self.shrinkage_checkpoints = np.logspace(-1, -10, 5, base=2)
 
     def _mean_error(self, predictions, labels):
@@ -79,12 +80,12 @@ class GBRT(object):
 
     def fit(self, train_set, test_set=None):
 
-        X, y = train_set.X, train_set.y
+        train_X, train_y = train_set.X, train_set.y
 
         reg_tree_ensemble = RegressionTreeEnsemble()
-        reg_tree_ensemble.set_initial_constant(np.mean(y))
+        reg_tree_ensemble.set_initial_constant(np.mean(train_y))
 
-        train_f_last = np.repeat(reg_tree_ensemble.c, len(X))
+        train_f_last = np.repeat(reg_tree_ensemble.c, len(train_X))
         train_errors, test_errors = [], []
 
         shrinkage_factor = self._shrinkage
@@ -92,23 +93,22 @@ class GBRT(object):
 
         for m in range(1, self._num_of_basis_functions):
 
-            samples = self._subsample(len(X))
-            train_X, train_y = X[samples], y[samples]
+            samples = self._subsample(len(train_X))
 
-            g_m = -(train_y - train_f_last[samples])
-            tree = self._cart.fit((train_X, g_m))
+            g_m = -(train_y[samples] - train_f_last[samples])
+            tree = self._cart.fit((train_X[samples], g_m))
 
             phi_of_x = np.array([tree.evaluate(x) for x in train_X])
-            beta_m = np.dot(-g_m, phi_of_x) / np.sum(np.power(phi_of_x, 2))
+            beta_m = np.dot(-g_m, phi_of_x[samples]) / np.sum(np.power(phi_of_x[samples], 2))
             reg_tree_ensemble.add_tree(tree, beta_m)
 
-            train_f_last[samples] += shrinkage_factor * beta_m * phi_of_x
+            train_f_last += shrinkage_factor * beta_m * phi_of_x
 
             if m in shrinkage_decay_checkpoints:
                 shrinkage_factor /= 2.0
                 print 'Shrinkage factor updated to: ', shrinkage_factor
 
-            train_errors.append(self._mean_error(train_f_last, y))
+            train_errors.append(self._mean_error(train_f_last, train_y))
             error_str = 'Learners: {:3d} | Train error: {:15.2f} |'.format(m, train_errors[-1])
 
             if test_set is not None:
